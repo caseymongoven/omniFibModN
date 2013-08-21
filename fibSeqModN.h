966d7eae926dbaf330ed7bc3e5bf391419908dfd
caseymongoven <cm@caseymongoven.com>
2013-08-20 17:16:37 -0700

    ADDED FILES
    ADDED FILES

---
+++ fibSeqModN.h
@@ -0,0 +1,256 @@
+#ifndef FIB_SEQ_MOD_N_H
+#define FIB_SEQ_MOD_N_H
+
+//check if all these headers are necessary
+#include <cmath>
+#include <vector>
+#include <list>
+#include <sstream>
+#include "allocore/io/al_App.hpp"
+
+#include "Gamma/SamplePlayer.h"
+
+using namespace std;
+using namespace al;
+
+
+class FibSeqModN : public SoundSource {
+    
+  public:
+  
+  	FibSeqModN(Vec3f seqPos_, const int g0_, const int g1_, const int mod_) : seqPos(seqPos_), g0(g0_), g1(g1_), mod(mod_) {
+  	
+
+  		level = .1; // level of samples being played
+  		
+		farClip(75.0); // distance at which amplitude reaches ampFar
+		ampFar(0.0); // amp at far clip
+
+		nearClip(0.0); // distance at which amplitude hits 1.0
+
+		rollOff(.000001); // not quite sure what this does
+  	
+  		currentPos = 0;
+  	
+      	stringstream ss;
+   		ss << mod;
+      	string sfile = "rim/"+ss.str()+".wav";
+      	
+    	play.load(sfile.c_str());
+
+		// generate the generalized Fibonacci sequence modulo n
+		if (mod == 1)  seq.push_back(0);
+		else {
+		  	seq.push_back(g0);
+			seq.push_back(g1);
+			seq.push_back(g0+g1);
+			int j = 3, v;
+			while (!((seq[j-2] == seq[0]) && (seq[j-1] == seq[1]))) {
+				v = (seq[j-1] + seq[j-2]) % mod;
+				seq.push_back(v); 
+				j++;
+			}
+			seq.erase(seq.end()-3, seq.end()-1);
+		}
+		
+		// calculate restricted period and number of restricted subsequences (for standard Fibonacci only)
+		resPeriod = seq.size();
+		for (int i = 0 ; i < (int)seq.size() ; i++) if (seq.at(i) == 0 && i > 0) {resPeriod = i; break;}
+		resSubseqs = 0;
+		for (int i = 0 ; i < (int)seq.size() ; i++) if (seq.at(i) == 0) {resSubseqs++;}
+		
+		// calculate the number of unique residues in the sequence
+		list<int> f_l((int)seq.size());
+		for (int i = 0 ; i < (int)seq.size() ; i++) f_l.push_back(seq[i]);
+		f_l.sort(); f_l.unique();
+		unique = f_l.size();
+		
+		// calculate apothem for regular polygon, plus cube radius
+		apothem = 0.5*tan(M_PI_2-M_PI/seq.size())+0.5;
+		if (mod == 1) apothem = 0.0;
+		
+		// calculate radius for regular polygon, plus altitude in between cubes
+		radius = 1.0/(2.0*sin(M_PI/seq.size()))+sin(M_PI-(M_PI_2/seq.size())/2.0-M_PI/2.0);
+		if (mod == 1) radius = sqrt(2.0)/2.0;
+		
+		// initialize the turn variable
+		turn = 0;
+	
+		// add a cube to the mesh
+		addCube(m, false, 0.5);
+		m.primitive(Graphics::TRIANGLES);
+		m.generateNormals();
+
+	}
+  
+  	
+  	virtual void onProcess(AudioIOData& io) {
+
+		while (io()) {
+			turn = play.pos();
+			currentPos = floor((turn/176400.0)*getPeriod());
+			writeSample(play()*level);
+		}
+
+	}
+	
+	virtual void onUpdateNav(){
+		Pose p;
+		//actual position causing strange sounds due to doppler shift interpolation deal
+		p.pos(getPosition() + Vec3f(0.0, currentMemberFloat(), apothem));
+		//p.pos(getPosition());
+		SoundSource::pose(p);
+	}
+
+
+    float currentMemberFloat();
+    
+  	void setPosition(Vec3f);
+  	Vec3f getPosition();
+  
+    int getPeriod();
+    int getUnique();
+    int getModulus();
+    float getApothem();
+    float getRadius(); 
+    int getRestrictedPeriod();
+    int getRestrictedSubsequences();
+    
+    void draw(Graphics&);
+    
+    virtual ~FibSeqModN() {};
+    
+    static int turn; 
+    
+  private:
+  
+    gam::SamplePlayer<float, gam::ipl::Linear, gam::phsInc::Loop> play;
+	vector<int> seq;
+	const int g0, g1, mod;
+	int unique, resPeriod, resSubseqs, currentPos;
+    Vec3f seqPos;
+    float apothem, radius, level;
+
+    Mesh m;
+
+
+
+};
+
+int FibSeqModN::turn = 0;
+
+float FibSeqModN::currentMemberFloat() {
+	return (float)seq[currentPos];
+}
+
+void FibSeqModN::setPosition(Vec3f seqPosition) {
+	this->seqPos = seqPosition;
+}
+Vec3f FibSeqModN::getPosition() {
+	return seqPos;
+}
+int FibSeqModN::getPeriod() {
+	return seq.size();
+}
+int FibSeqModN::getUnique() {
+	return unique;
+}
+int FibSeqModN::getModulus() {
+	return mod;
+}
+float FibSeqModN::getApothem() {
+	return apothem;
+}
+float FibSeqModN::getRadius() {
+	return radius;
+}
+int FibSeqModN::getRestrictedPeriod() {
+	return resPeriod;
+}
+int FibSeqModN::getRestrictedSubsequences() {
+	return resSubseqs;
+}
+
+
+void FibSeqModN::draw(Graphics& g) {
+    	
+    	
+    	
+	// this sets the placement of the entire seq
+	g.pushMatrix();
+	g.translate(seqPos);
+	
+	// this sets the placement of all the cubes
+	g.pushMatrix();
+	g.translate(0.0, 0.0, apothem);
+		
+	float t_fl = 360.0*(turn/(44100.0*4.0));
+		
+	for (int i = 0; i < (int)seq.size(); i++) {
+		g.pushMatrix();
+		g.translate(0.0, seq[i], -apothem);
+		g.rotate(((float)i/seq.size())*360.0+(360.0/seq.size())/2-t_fl, 0.0, 1.0, 0.0);
+		g.translate(0.0, 0, apothem);
+		if (t_fl-((float)i/seq.size())*360.0 >= 0.0 && t_fl-((float)i/seq.size())*360.0 < 360.0/seq.size()) {
+			g.color(HSV(0, 1, 1));
+		}
+		else g.color(HSV(0, 0, 1));
+		g.draw(m);
+		g.popMatrix();
+	}
+	
+	g.popMatrix();
+	g.popMatrix();
+	
+}
+
+
+// Helper functions:
+
+vector< vector<FibSeqModN*> > categorizeByRestrictedSubseqs(vector<FibSeqModN*> seqs) {
+
+	vector< vector<FibSeqModN*> > categorizedSeqs(3);
+	
+	for (int i = 0 ; i < (int)seqs.size() ; i++) {
+		if (seqs.at(i)->getRestrictedSubsequences() == 1) categorizedSeqs.at(0).push_back(seqs.at(i));
+		else if (seqs.at(i)->getRestrictedSubsequences() == 2) categorizedSeqs.at(1).push_back(seqs.at(i));
+		else if (seqs.at(i)->getRestrictedSubsequences() == 4) categorizedSeqs.at(2).push_back(seqs.at(i));
+	}
+	
+	return categorizedSeqs;
+}
+
+vector<FibSeqModN*> orderByPeriodLength(vector<FibSeqModN*> seqs) {
+
+	vector<FibSeqModN*> reorderedSeqs;
+
+	vector<int> periods;
+	for (int i = 0 ; i < (int)seqs.size() ; i++) periods.push_back(seqs.at(i)->getPeriod());
+	sort(periods.begin(), periods.end());
+	
+	vector<int>::iterator it;
+    it = unique(periods.begin(), periods.end());
+	periods.resize( distance(periods.begin(),it) ); 
+	
+	for (int i = 0 ; i < (int)periods.size() ; i++) {
+		for (int j = 0 ; j < (int)seqs.size() ; j++) {
+			if (seqs.at(j)->getPeriod() == periods.at(i)) reorderedSeqs.push_back(seqs.at(j));
+		} 
+	}
+
+	return reorderedSeqs;
+
+}
+
+float getLargestRadius(vector<FibSeqModN*> seqs) {
+	
+	float largest = 0.0;
+	
+	for (int i = 0 ; i < (int)seqs.size() ; i++) {
+		if (seqs.at(i)->getRadius() > largest) largest = seqs.at(i)->getRadius();
+	}
+
+	return largest;
+	
+}
+#endif
---
+++ omniFibModN.cpp
@@ -0,0 +1,332 @@
+#include "alloutil/al_OmniApp.hpp"
+#include "allocore/al_Allocore.hpp"
+#include <assert.h>
+#include <cmath>
+#include <vector>
+#include "fibSeqModN.h"
+using namespace al;
+
+const int G0 = 0;
+const int G1 = 1;
+const int NUMSEQS = 34;
+const float GAP = 10.5;
+
+static std::string host = Socket::hostName();
+
+Listener * listener;
+Nav navMaster(Vec3d(0,0,-40), 0.95);
+Stereographic stereo;
+#define AUDIO_BLOCK_SIZE 256
+
+AudioScene scene(3, 3, AUDIO_BLOCK_SIZE);
+const int numSpeakers = 54;
+const double topEl = 30.0; // (or 45???)
+const double botEl =-30.0; // (or -45???)
+Speaker speakers[numSpeakers] = {
+	Speaker( 1-1, 270.0, topEl),
+	Speaker( 2-1, 300.0, topEl),
+	Speaker(3-1, 330.0, topEl),
+	Speaker(4-1, 0.0, topEl),
+	Speaker(5-1,30.0, topEl),
+	Speaker(6-1,60.0, topEl),
+	Speaker(7-1,90.0, topEl),
+	Speaker(8-1,120.0, topEl),
+	Speaker(9-1,150.0, topEl),
+	Speaker(10-1,180.0, topEl),
+	Speaker(11-1,210.0, topEl),
+	Speaker(12-1,240.0, topEl),
+	
+	Speaker(13-1, 288.006994221339, 0.0),
+	Speaker(14-1, 299.02687287475, 0.0),
+	Speaker(15-1, 309.69049620981, 0.0),
+	Speaker(16-1, 320.070952103726, 0.0),
+	Speaker(17-1, 330.234430050552, 0.0),
+	Speaker(18-1, 340.240917285025, 0.0),
+	Speaker(19-1, 350.145513893009, 0.0),
+	Speaker(20-1, 0.0, 0.0),
+	Speaker(21-1, 9.85448610699111, 0.0),
+	Speaker(22-1, 19.7590827149751, 0.0),
+	Speaker(23-1, 29.7655699494475, 0.0),
+	Speaker(24-1, 39.9290478962744, 0.0),
+	Speaker(25-1, 50.3095037901896, 0.0),
+	Speaker(26-1, 60.9731271252495, 0.0),
+	Speaker(27-1, 71.9930057786609, 0.0),
+	Speaker(28-1, 108.006994221339, 0.0),
+	Speaker(29-1, 119.02687287475, 0.0),
+	Speaker(30-1, 129.69049620981, 0.0),
+	Speaker(31-1, 140.070952103726, 0.0),
+	Speaker(32-1, 150.234430050552, 0.0),
+	Speaker(33-1, 160.240917285025, 0.0),
+	Speaker(34-1, 170.145513893009, 0.0),
+	Speaker(35-1, 180.0, 0.0),
+	Speaker(36-1, 189.854486106991, 0.0),
+	Speaker(37-1, 199.759082714975, 0.0),
+	Speaker(38-1, 209.765569949448, 0.0),
+	Speaker(39-1, 219.929047896274, 0.0),
+	Speaker(40-1, 230.30950379019, 0.0),
+	Speaker(41-1, 240.97312712525, 0.0),
+	Speaker(42-1, 251.993005778661, 0.0),
+	
+	Speaker(43-1, 270.0, botEl),
+	Speaker(44-1, 300.0, botEl),
+	Speaker(45-1, 330.0, botEl),
+	Speaker(46-1, 0.0, botEl),
+	Speaker(47-1,30.0, botEl),
+	Speaker(48-1,60.0, botEl),
+	Speaker(49-1,90.0, botEl),
+	Speaker(50-1,120.0, botEl),
+	Speaker(51-1,150.0, botEl),
+	Speaker(52-1,180.0, botEl),
+	Speaker(53-1,210.0, botEl),
+	Speaker(54-1,240.0, botEl),
+	
+};
+
+
+struct PacketData{
+	PacketData(): location_x(1), location_y(1), location_z(1), turn(1), quat_w(1), quat_x(1), quat_y(1), quat_z(1){}
+	float location_x;
+	float location_y;
+	float location_z;
+	int turn;
+	float quat_w;
+	float quat_x;
+	float quat_y;
+	float quat_z;
+	
+	void clear(){ location_x=0; location_y=0; location_z=0; turn=0; quat_w = 0; quat_x = 0; quat_y = 0; quat_z = 0; }
+	bool valid() const { return true; }
+	void print() const { printf("%g %g %g %x %g %g %g %g\n", location_x, location_y, location_z, turn, quat_w, quat_x, quat_y, quat_z); }
+};
+
+// global variables for state synchronization
+float l_x, l_y, l_z, q_w, q_x, q_y, q_z;
+int t;
+
+struct OSCHandler : public osc::PacketHandler{
+	void onMessage(osc::Message& m){
+	
+		assert(m.typeTags() == "fffiffff");
+		assert(m.addressPattern() == "/test");
+
+		std::string s;
+		PacketData d;
+
+		d.clear();
+		m >> d.location_x >> d.location_y >> d.location_z >> d.turn >> d.quat_w >> d.quat_x >> d.quat_y >> d.quat_z;
+
+		l_x = d.location_x;
+		l_y = d.location_y;
+		l_z = d.location_z;
+		t = d.turn;
+		q_w = d.quat_w;
+		q_x = d.quat_x;
+		q_y = d.quat_y;
+		q_z = d.quat_z;
+		
+		assert(d.valid());
+	}
+} handler;
+
+
+struct MyApp : OmniApp {
+  bool iAmTheMaster;
+  Material material;
+  Light light;
+
+  vector<Vec3f> seqPosition;
+  vector<FibSeqModN*> fibSeqModN;
+  vector< vector<FibSeqModN*> > reorderedSeqs;
+	
+  vector< vector<int> > seqs;
+	
+  vector<osc::Send*> s;
+  osc::Recv* r;
+	
+  PacketData data;
+
+  MyApp() {
+  
+    cout << "My hostname is " << host << endl;
+    if (host == "bossanova.local") {
+    
+    	audioIO().deviceOut(AudioDevice("AF12 x5b"));
+    	audioIO().channelsOut(60);
+    
+    	audioIO().print();
+    
+		cout << "I am the master!" << endl;
+		iAmTheMaster = true;
+	
+		s.resize(12);
+		s[0] = new osc::Send(11111, "spherez01.mat.ucsb.edu");
+		s[1] = new osc::Send(11111, "spherez02.mat.ucsb.edu");
+		s[2] = new osc::Send(11111, "spherez03.mat.ucsb.edu");
+		s[3] = new osc::Send(11111, "spherez04.mat.ucsb.edu");
+		s[4] = new osc::Send(11111, "spherez05.mat.ucsb.edu");
+		s[5] = new osc::Send(11111, "spherez07.mat.ucsb.edu");
+		s[6] = new osc::Send(11111, "gr01.mat.ucsb.edu");
+		s[7] = new osc::Send(11111, "gr04.mat.ucsb.edu");
+		s[8] = new osc::Send(11111, "gr05.mat.ucsb.edu");
+		s[9] = new osc::Send(11111, "gr06.mat.ucsb.edu");
+		s[10] = new osc::Send(11111, "gr08.mat.ucsb.edu");
+		s[11] = new osc::Send(11111, "photon.mat.ucsb.edu");
+		  
+    } else {
+		cout << "I am not the master." << endl;
+		iAmTheMaster = false;
+
+		r = new osc::Recv(11111);
+		r->handler(handler);
+
+		r->timeout(0.1); 
+		r->start();
+    
+    }
+
+
+    lens().far(600.0); 
+    nav().pos(0, 0, 0);
+
+	for (int i = 0 ; i < NUMSEQS ; i++) {
+		seqPosition.push_back(Vec3f(0.0, 0.0, 0.0));
+		fibSeqModN.push_back(new FibSeqModN(seqPosition[i], G0, G1, i+1));
+	}
+
+	reorderedSeqs = categorizeByRestrictedSubseqs(fibSeqModN);
+
+	reorderedSeqs.at(0) = orderByPeriodLength(reorderedSeqs.at(0));
+	reorderedSeqs.at(1) = orderByPeriodLength(reorderedSeqs.at(1));
+	reorderedSeqs.at(2) = orderByPeriodLength(reorderedSeqs.at(2));
+
+	float radius0 = 0.0;
+	float largestRadius0 = getLargestRadius(reorderedSeqs.at(0))+getLargestRadius(reorderedSeqs.at(1));
+	for (int i = 0 ; i < (int)reorderedSeqs.at(0).size() ; i++) {
+		reorderedSeqs.at(0).at(i)->setPosition(Vec3f(-largestRadius0, 0.0, radius0));
+		if (i < (int)reorderedSeqs.at(0).size()-1) radius0 += reorderedSeqs.at(0).at(i)->getRadius()+reorderedSeqs.at(0).at(i+1)->getRadius()+GAP;
+		scene.addSource(*reorderedSeqs.at(0).at(i));
+	}
+
+	float radius1 = 0.0;
+	for (int i = 0 ; i < (int)reorderedSeqs.at(1).size() ; i++) {
+		reorderedSeqs.at(1).at(i)->setPosition(Vec3f(0.0, 0.0, radius1));
+		if (i < (int)reorderedSeqs.at(1).size()-1) radius1 += reorderedSeqs.at(1).at(i)->getRadius()+reorderedSeqs.at(1).at(i+1)->getRadius()+GAP;
+		scene.addSource(*reorderedSeqs.at(1).at(i));
+	}
+
+	float radius2 = 0.0;
+	float largestRadius2 = getLargestRadius(reorderedSeqs.at(1))+getLargestRadius(reorderedSeqs.at(2));
+	for (int i = 0 ; i < (int)reorderedSeqs.at(2).size() ; i++) {
+		reorderedSeqs.at(2).at(i)->setPosition(Vec3f(largestRadius2, 0.0, radius2));
+		if (i < (int)reorderedSeqs.at(2).size()-1) radius2 += reorderedSeqs.at(2).at(i)->getRadius()+reorderedSeqs.at(2).at(i+1)->getRadius()+GAP;
+		scene.addSource(*reorderedSeqs.at(2).at(i));
+	}
+
+	listener = scene.createListener(2);
+
+	listener->numSpeakers(numSpeakers);
+	for(int i=0; i<numSpeakers; ++i){
+		listener->speakerPos(
+			i,
+			speakers[i].deviceChannel,
+			speakers[i].azimuth,
+			speakers[i].elevation
+		);
+	}
+
+	gam::Sync::master().spu(audioIO().fps());
+	initWindow();
+	initAudio();
+  }
+
+  virtual ~MyApp() {}
+
+  virtual void onAnimate(al_sec dt) {
+
+    if (iAmTheMaster) {
+    
+		data.location_x = nav().pos()[0];
+		data.location_y = nav().pos()[1];
+		data.location_z = nav().pos()[2];
+		data.turn = FibSeqModN::turn;
+		data.quat_w = nav().quat()[0];
+		data.quat_x = nav().quat()[1];
+		data.quat_y = nav().quat()[2];
+		data.quat_z = nav().quat()[3];
+		
+		for (int i = 0 ; i < 12 ; i++) {
+			s[i]->clear();
+			s[i]->beginBundle(0);
+			s[i]->beginMessage("/test");
+			*s[i] << data.location_x << data.location_y << data.location_z << data.turn << data.quat_w << data.quat_x << data.quat_y << data.quat_z;
+			s[i]->endMessage();
+			s[i]->endBundle();
+			s[i]->send();
+		}
+		
+    } else {
+    	  
+		nav().pos(l_x, l_y, l_z);
+		FibSeqModN::turn = t;
+		nav().quat()[0] = q_w;
+		nav().quat()[1] = q_x;
+		nav().quat()[2] = q_y;
+		nav().quat()[3] = q_z;
+    		
+    }
+  }
+  
+  virtual void onDraw(Graphics& g) {
+	material();
+	light();
+	
+	light.pos(nav().pos());
+	
+	for (int i = 0 ; i < (int)reorderedSeqs.at(0).size() ; i++) reorderedSeqs.at(0).at(i)->draw(g);
+	for (int i = 0 ; i < (int)reorderedSeqs.at(1).size() ; i++) reorderedSeqs.at(1).at(i)->draw(g);
+	for (int i = 0 ; i < (int)reorderedSeqs.at(2).size() ; i++) reorderedSeqs.at(2).at(i)->draw(g);
+		
+  }
+
+  virtual void onSound(al::AudioIOData& io) {
+    if (iAmTheMaster) {
+    
+    	int numFrames = io.framesPerBuffer();
+
+		for (int i = 0 ; i < (int)reorderedSeqs.at(0).size() ; i++) {
+			io.frame(0);
+			reorderedSeqs.at(0).at(i)->onUpdateNav();
+			reorderedSeqs.at(0).at(i)->onProcess(io);
+		}
+		for (int i = 0 ; i < (int)reorderedSeqs.at(1).size() ; i++) {
+			io.frame(0);
+			reorderedSeqs.at(1).at(i)->onUpdateNav();
+			reorderedSeqs.at(1).at(i)->onProcess(io);
+		}
+		for (int i = 0 ; i < (int)reorderedSeqs.at(2).size() ; i++) {
+			io.frame(0);
+			reorderedSeqs.at(2).at(i)->onUpdateNav();
+			reorderedSeqs.at(2).at(i)->onProcess(io);
+		}
+		
+		navMaster.step(0.5);
+
+		listener->pose(nav());
+
+		scene.encode(numFrames, io.framesPerSecond());
+		scene.render(&io.out(0,0), numFrames);
+		
+
+	}
+  }
+
+  virtual bool onKeyDown(const Keyboard& k){
+    return true;
+  }
+};
+
+int main(int argc, char * argv[]) {
+
+    MyApp().start();
+    return 0;
+}
